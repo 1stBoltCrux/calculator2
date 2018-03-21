34e9eac47f2ed62aece46222aa25207f13337a37
Isaac Shoman and Ted Leary <ted@>
2018-03-21 10:24:43 -0700

    Add division functionality to UI and html

--- js/scripts.js
+++ js/scripts.js
@@ -36,16 +36,20 @@ $(function() {
     $("#output2").text(result);
   });
 
+    $("form#mult").submit(function(event) {
+      event.preventDefault();
+      var number1 = parseInt($("#mult1").val());
+      var number2 = parseInt($("#mult2").val());
+      var result = multiply(number1, number2);
+      $("#output3").text(result);
+    });
 
-  // var number1 = parseInt(("Enter a number:"));
-  // var number2 = parseInt(("Enter another number:"));
-  // var result = subtract(number1, number2);
-  //
-  // var number1 = parseInt(("Enter a number:"));
-  // var number2 = parseInt(("Enter another number:"));
-  // var result = divide(number1, number2);
-  //
-  // var number1 = parseInt(("Enter a number:"));
-  // var number2 = parseInt(("Enter another number:"));
-  // var result = multiply(number1, number2);
-});
+    $("form#divide").submit(function(event) {
+      event.preventDefault();
+      var number1 = parseInt($("#div1").val());
+      var number2 = parseInt($("#div2").val());
+      var result = divide(number1, number2);
+      $("#output4").text(result);
+    });
+
+  });
